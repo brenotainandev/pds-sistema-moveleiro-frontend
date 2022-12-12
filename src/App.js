28f5fc9f9362b7ccd53b9ffb2c65a5dec20c20a0
Breno Tainan <brenotaguiar@gmail.com>
2022-12-11 23:27:03 -0300

    feat: crud produto finalizado

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
-import GlobalStyle from "./styles/global";
-import styled from "styled-components";
-import Form from "./components/Form.js";
-import Grid from "./components/Grid";
-import { useEffect, useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
-
-const Container = styled.div`
-  width: 100%;
-  max-width: 95%;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-`;
-
-const Title = styled.h2``;
+import React from 'react';
+import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import ProductPage from './pages/Product';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [onEdit, setOnEdit] = useState(null);
-
-  const getProducts = async () => {
-    try {
-      const res = await axios.get("http://localhost:3001/product");
-      setProducts(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
-    } catch (error) {
-      toast.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, [setProducts]);
-
   return (
-    <>
-      <Container>
-        <Title>PRODUTOS</Title>
-        <Form onEdit={onEdit} setOnEdit={setOnEdit} getProducts={getProducts} />
-        <Grid setOnEdit={setOnEdit} products={products} setProducts={setProducts}/>
-      </Container>
-      <ToastContainer autoClose={3000} position={toast.POSITION.TOP_CENTER} />
-      <GlobalStyle />
-    </>
+
+    <Switch>
+      <Redirect exact from="/" to="/product" />
+      <Route path="/product" component={ ProductPage } />
+    </Switch>
+
   );
 }
 
